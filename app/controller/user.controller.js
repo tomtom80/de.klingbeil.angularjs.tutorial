19eafa3cabaeb37afd739f4a3396c2b43820a1a5
tomtom80 <tom.klingbeil@gmail.com>
2016-07-15 16:24:39 +0200

    step 9

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,5 +1,11 @@
+function User(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
 app.controller( 'userController', function () {
     this.otherName = "The old Name"
+    this.users = [new User("tom","klingbeil"), new User("ralf", "engel"), new User("viktor","Hess")];
     this.changeName = function () {
         this.otherName = "The New Name";
     }
